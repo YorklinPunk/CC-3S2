3da95a9c222ad73181ae167a43797d0487713909
YorklinPunk <122409047+YorklinPunk@users.noreply.github.com>
2023-04-06 10:32:55 -0500

    Add files via upload

---
+++ Actividades/TDD-2/src/test/java/com/wordz/domain/Word.java
@@ -0,0 +1,12 @@
+package com.wordz.domain;
+
+public class Word {
+    public Word(String correctWord) {
+        // No Implementado
+    }
+    public Score guess(String attempt) {
+        var score = new Score();
+        return score;
+    }
+}
+
