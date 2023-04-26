f1bad9a07bf4dadaab953f35959aef018ab6096f
YorklinPunk <122409047+YorklinPunk@users.noreply.github.com>
2023-04-26 09:56:17 -0500

    Add files via upload

---
+++ Proyecto-3S2/Sprint.0/SosProyect/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SosProyect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox9);
+            this.panel1.Controls.Add(this.textBox8);
+            this.panel1.Controls.Add(this.textBox7);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(81, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(368, 246);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(168, 159);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(32, 20);
+            this.textBox9.TabIndex = 7;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(96, 159);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(32, 20);
+            this.textBox8.TabIndex = 6;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(238, 159);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(32, 20);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(238, 101);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(32, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(168, 101);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(32, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(96, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(32, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(238, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(32, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public string insertarLetter( string letter)
+        {
+            if (letter != "S" || letter != "O")
+            {
+                return "Letra inválida";
+            }
+            return letter;
+        }
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Proyecto-3S2/Sprint.0/SosProyect/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System.Windows.Forms;
+using SosPruebaNUnit;
+
+namespace SosProyect
+{
+    public partial class Form1 : Form
+    {
+        string resultado = "";
+        string valorInput = "";
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Juego SOS";
+            button1.Text = "Iniciar Juego";
+            label2.Text = "Turno Jugador 3";
+            label3.Text = "Turno Jugador 2";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox1.Text;
+            resultado = Class1.insertLetter(textBox1.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox1.Text = "";
+                MessageBox.Show(resultado);
+                textBox1.Focus();
+            }
+            else
+            {
+                textBox1.Enabled = false;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox2.Text;
+            resultado = Class1.insertLetter(textBox2.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox2.Text = "";
+                MessageBox.Show(resultado);
+                textBox1.Focus();
+            }
+            else
+            {
+                textBox2.Enabled = false;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox3.Text;
+            resultado = Class1.insertLetter(textBox3.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox3.Text = "";
+                MessageBox.Show(resultado);
+                textBox3.Focus();
+            }
+            else
+            {
+                textBox3.Enabled = false;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox4.Text;
+            resultado = Class1.insertLetter(textBox4.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox4.Text = "";
+                MessageBox.Show(resultado);
+                textBox4.Focus();
+            }
+            else
+            {
+                textBox4.Enabled = false;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox5.Text;
+            resultado = Class1.insertLetter(textBox5.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox5.Text = "";
+                MessageBox.Show(resultado);
+                textBox5.Focus();
+            }
+            else
+            {
+                textBox5.Enabled = false;
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox6.Text;
+            resultado = Class1.insertLetter(textBox6.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox6.Text = "";
+                MessageBox.Show(resultado);
+                textBox6.Focus();
+            }
+            else
+            {
+                textBox6.Enabled = false;
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox7.Text;
+            resultado = Class1.insertLetter(textBox7.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox7.Text = "";
+                MessageBox.Show(resultado);
+                textBox7.Focus();
+            }
+            else
+            {
+                textBox7.Enabled = false;
+            }
+        }
+
+        private void textBox8_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox8.Text;
+            resultado = Class1.insertLetter(textBox8.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox8.Text = "";
+                MessageBox.Show(resultado);
+                textBox8.Focus();
+            }
+            else
+            {
+                textBox8.Enabled = false;
+            }
+        }
+
+        private void textBox9_TextChanged(object sender, System.EventArgs e)
+        {
+            valorInput = textBox9.Text;
+            resultado = Class1.insertLetter(textBox9.Text);
+            if (resultado == "Letra inválida")
+            {
+                textBox9.Text = "";
+                MessageBox.Show(resultado);
+                textBox9.Focus();
+            }
+            else
+            {
+                textBox9.Enabled = false;
+            }
+        }
+
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+        }
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, System.EventArgs e)
+        {
+        }
+        private void label3_Click(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
