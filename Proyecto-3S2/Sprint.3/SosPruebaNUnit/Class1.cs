86a9d11a6dcafc99ebe4721884586e0fe7a138cd
YorklinPunk <122409047+YorklinPunk@users.noreply.github.com>
2023-05-12 17:40:28 -0500

    codigo
    2da parte

---
+++ Proyecto-3S2/Sprint.3/SosPruebaNUnit/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace SosPruebaNUnit
+{
+    public class Class1
+    {
+        public static string insertLetter(string letter)
+        {
+            if (letter == "S" || letter == "O")
+            {
+                return letter;
+            }
+            return "Letra inválida";
+        }
+    }
+}
---
+++ Proyecto-3S2/Sprint.3/SosPruebaNUnitTests/Class1Tests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SosPruebaNUnit.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        //PRUEBA CUANDO ES O 
+        public void insertarLetterOTest()
+        {
+            string result = Class1.insertLetter("O");
+            Assert.AreEqual("O", result);
+        }
+
+        [TestMethod()]
+        //PRUEBA CUANDO ES S 
+        public void insertarLetterSTest()
+        {
+            string result = Class1.insertLetter("S");
+            Assert.AreEqual("S", result);
+        }
+
+        [TestMethod()]
+        //PRUEBA CUANDO ES LETRA INVALIDA 
+        public void insertarInvalidLetterTest()
+        {
+            string result = Class1.insertLetter("X");
+            Assert.AreEqual("Letra inválida", result);
+        }
+    }
+}
